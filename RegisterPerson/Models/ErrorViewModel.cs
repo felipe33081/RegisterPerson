cce88cbb59e444c5d7427f0c6e053643419c7571
felipe33081 <79608511+felipe33081@users.noreply.github.com>
2021-07-08 16:53:04 -0300

    Project Created

---
+++ RegisterPerson/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RegisterPerson.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
